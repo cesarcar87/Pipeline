1d07d76cb0e8ed7750e7a5ac3fd8c82a599070ca
Cesar Caraballo <tticesarcar@gmail.com>
2022-09-14 14:03:33 -0300

    Primer commit pipelin

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '996iie',
+  viewportWidth: 1500,
+  viewportHeight: 900,
+  chromeWebSecurity: false,
+  pageLoadTimeout: 9000, //Tiempo máximo de espera de carga de cada página (global) → 9s
+  defaultCommandTimeout:10000, //Tiempo máximo de espera de carga de cada elemento (global) → 10s
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
